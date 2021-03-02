7339bbc8d40af84f6813cc5fe727dba8363c4dd5
bayutirto <bang.bayutirto@gmail.com>
2021-03-02 12:25:54 +0700

    create auth controller logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,65 @@
-exports.register = (req, res, next) => {
-    res.send("Register Route");
+const User = require('../models/User');
+
+exports.register = async (req, res, next) => {
+    const {username, email, password } = req.body;
+
+    try {
+        const user = await User.create({
+            username, email, password,
+        });
+
+        res.status(201).json({
+            success: true,
+            user
+        });
+    } catch(error) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
 }
 
-exports.login = (req, res, next) => {
-    res.send("Login Route");
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        res.status(400).json({
+            success: false,
+            error: "Please provide email and password"
+        });
+    }
+
+    try {
+        const user = await User.findOne({ email }).select("+password");
+
+        if(!User) {
+            res.status(400).json({
+                success: false,
+                error: "Invalid credentials"
+            });
+        }
+
+        const isMatch = await user.matchPasswords(password);
+
+        if(!isMatch){
+            res.status(404).json({
+                success: false,
+                error: "invalid credentials"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            token: "asdasdasd"
+        })
+
+    } catch(error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
 }
 
 exports.forgotpassword = (req, res, next) => {
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 require('dotenv').config({path: "./config.env"});
-
 const express = require("express");
+const connectDB = require('./config/db');
+
+//Connect DB
+connectDB();
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/v1/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Logged Error: ${err}`);
+    server.close(() => process.exit(1));
+})
